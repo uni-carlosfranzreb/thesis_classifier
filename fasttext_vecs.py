0951fe146e733eb18b7fa4b9e9fab3f939a74fe9
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-01-29 13:49:22 +0100

    Gather pretrained vectors for words in the vocab.

--- fasttext_vecs.py
+++ fasttext_vecs.py
@@ -5,6 +5,7 @@ lower-cased words. """
 
 import json
 import io
+import logging
 
 
 def load_vectors(fname):
@@ -70,22 +71,69 @@ def remove_upper(old_file, new_file):
   be updated after the procedure. """
   old = io.open(old_file, encoding='utf-8', newline='\n', errors='ignore')
   new = open(new_file, 'a', encoding='utf-8')
+  low_words = get_lower(old_file)
   for line in old:
-    if line[0].isupper():
+    if line[0].isupper():  # first char of line is upper-case
       tokens = line.rstrip().split(' ')
       low_token = tokens[0].lower()
-      if find_vec(old_file, low_token) is None:
+      if low_token not in low_words:
+        logging.info(f'{tokens[0]} has no lower-cased equivalent')
         low_line = ' '.join([low_token] + tokens[1:])
         new.write(low_line)
     else:
       new.write(line)
-  lines = open(new_file).readlines()
+  new.close()
+  lines = open(new_file, encoding='utf-8').readlines()
+  logging.info(f'New file has {len(lines)-1} words')
   lines[0] = f'{len(lines)-1} {lines[0].rstrip().split(" ")[1]}'
-  with open(new_file, 'w') as f:
+  with open(new_file, 'w', encoding='utf-8') as f:
     f.writelines(lines)
 
 
+def get_lower(fname):
+  """Return all the lower words in the given IO object as a list. """
+  vecs = io.open(fname, encoding='utf-8', newline='\n', errors='ignore')
+  words = []
+  for line in vecs:
+    if not line[0].isupper():
+      words.append(line.rstrip().split(' ')[0])
+  logging.info(f'{len(words)} non-capitalized words found in the file')
+  return words
+
+
+def vocab_vectors(vecs_file, vocab_file, dump_file):
+  """ Dump all vectors whose words are in the vocab. The first line of the
+  file shows the number of entries and the number of embeddings. It has to 
+  be updated after the procedure."""
+  vecs = io.open(vecs_file, encoding='utf-8', newline='\n', errors='ignore')
+  vocab = list(json.load(open(vocab_file)).keys())
+  logging.info(f'Vocab has {len(vocab)} words')
+  dump = open(dump_file, 'a', encoding='utf-8')
+  dump.write(vecs.readline())  # dump first line with file info.
+  for line in vecs:
+    word = line.split(' ')[0]
+    if word in vocab:
+      vocab.remove(word)
+      dump.write(line)
+  dump.close()
+  logging.info(vocab)
+  logging.info(f'{len(vocab)} vocab words are not in the vector file')
+  lines = open(dump_file, encoding='utf-8').readlines()
+  logging.info(f'{len(lines)-1} lines in the new file')
+  lines[0] = f'{len(lines)-1} {lines[0].rstrip().split(" ")[1]}'
+  with open(dump_file, 'w', encoding='utf-8') as f:
+    f.writelines(lines)
+  
+
 if __name__ == '__main__':
   vecs_file = 'data/pretrained_vecs/wiki-news-300d-1M-subword.vec'
-  new_file = 'data/pretrained_vecs/lower.vec'
-  remove_upper(vecs_file, new_file)
+  lower_file = 'data/pretrained_vecs/lower_vecs.vec'
+  vocab_file = 'data/pretrained_vecs/vocab_vecs.vec'
+  vocab_json = 'data/vocab/vocab.json'
+  logging.basicConfig(
+    level=logging.INFO, 
+    handlers=[logging.FileHandler('logs/vocab_vectors.log', 'w', 'utf-8')],
+    format='%(message)s'
+  )
+  vocab_vectors(vecs_file, vocab_json, vocab_file)
+  # vocab_vectors(lower_file, vocab_file, dump_file)
