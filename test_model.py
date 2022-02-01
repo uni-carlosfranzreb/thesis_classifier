2f9472119d6d24602c41f1b9168fad567d602d57
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-01 21:55:49 +0100

    Add script to test the model works properly

---
+++ test_model.py
@@ -0,0 +1,20 @@
+""" Test the classifier neural network. """
+
+
+import torch
+
+from classifier.model import Classifier
+
+
+def test_forward(n_subjects, batch_size, n_words, n_dims):
+  x = torch.rand((batch_size, n_words, n_dims))
+  model = Classifier(n_subjects, n_dims)
+  model(x)
+
+
+if __name__ == '__main__':
+  n_subjects = 1200
+  batch_size=10
+  n_words = 400
+  n_dims = 300
+  test_forward(n_subjects, batch_size, n_words, n_dims)
