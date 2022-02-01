5a207211921ea191c105fd12a32a4b4f072e2204
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-01 20:13:39 +0100

    Create script to train the classifier

---
+++ train_model.py
@@ -0,0 +1,29 @@
+""" Train the classifier neural network. """
+
+
+from time import time
+import logging
+
+from torch.nn import BCELoss
+
+from classifier.train import init_training
+
+
+if __name__ == '__main__':
+  run_id = int(time())
+  docs_folder = 'data/openalex/doc_vecs'
+  subjects_file = 'data/openalex/subjects.json'
+  n_words = 400
+  n_dims = 300
+  loss = BCELoss
+  batch_size = 10
+  n_epochs = 20
+  lr = .1
+  momentum = .5
+  logging.basicConfig(
+    level=logging.INFO,
+    filename=f'logs/training_{run_id}.log'
+  )
+  init_training(run_id, docs_folder, subjects_file, n_words, n_dims, loss,
+    batch_size, n_epochs, lr, momentum
+  )
