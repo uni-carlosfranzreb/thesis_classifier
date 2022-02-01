ffc43ff0252080c7c31e7d3144413d1072a1607e
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-01 22:11:30 +0100

    initialize loss class

--- train_model.py
+++ train_model.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
   subjects_file = 'data/openalex/subjects.json'
   n_words = 400
   n_dims = 300
-  loss = BCELoss
+  loss = BCELoss()
   batch_size = 10
   n_epochs = 20
   lr = .1
