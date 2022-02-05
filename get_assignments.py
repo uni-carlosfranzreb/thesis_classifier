c4d25ff838286914058830e7ee8cb3ad8c8641a3
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-05 18:31:33 +0100

    Iterate over folder and evaluate data with model

--- get_assignments.py
+++ get_assignments.py
@@ -5,36 +5,41 @@ fasttext vectors for the texts (see vectorize_repos in process_docs.py """
 
 import json
 import torch
+from os import listdir
 
 from classifier.model import Classifier
 
 
 def compute(model_file, dump_file, n_words=400, n_dims=300):
-  data = json.load(open(
-    'data/json/dim/all/data_fasttext.json', encoding='utf-8'
-  ))
+  data_folder = 'data/pretrained_vecs/data'
   subjects = list(json.load(open('data/openalex/subjects.json')).keys())
   model = Classifier(len(subjects), n_dims)
-  model.load_state_dict(torch.load(model_file))
+  model.load_state_dict(
+    torch.load(model_file, map_location=torch.device('cpu'))
+  )
+  model.eval()  # deactivate dropout
   assigned = {}
-  for doc, vecs in data.items():
-    assigned[doc] = {}
-    data = prepare_data(vecs, n_words, n_dims)
-    out = model(data)
-    for i in range(len(subjects)):
-      assigned[doc][subjects[i]] = out[i]
+  for file in listdir(data_folder):
+    data = json.load(open(f'{data_folder}/{file}'))
+    for doc, vecs in data.items():
+      assigned[doc] = {}
+      data = prepare_data(vecs, n_words, n_dims)
+      with torch.no_grad():
+        out = model(data).squeeze()
+      for i in range(len(subjects)):
+        assigned[doc][subjects[i]] = out[i].item()
   json.dump(assigned, open(dump_file, 'w'))
 
 
-def prepare_data(doc, n_words, n_dims):
+def prepare_data(data, n_words, n_dims):
   """ Resize vectors to fit the number of words, as done in load_data.py. """
-  all_data = torch.tensor(doc['data'])
+  all_data = torch.tensor(data)
   if all_data.shape[0] >= n_words:
     data = all_data[:n_words]
   else:
     data = torch.zeros(n_words, n_dims)
     data[:all_data.shape[0]] = all_data
-  return data
+  return data.unsqueeze(0)
   
 
 if __name__ == '__main__':
@@ -43,4 +48,4 @@ if __name__ == '__main__':
   gargiulo_dump = 'data/classifiers/1643821400/probabilities.json'
   baruch_dump = 'data/classifiers/1643982969/probabilities.json'
   compute(gargiulo_model, gargiulo_dump)
-  compute(baruch_model, baruch_dump)
+  # compute(baruch_model, baruch_dump)
