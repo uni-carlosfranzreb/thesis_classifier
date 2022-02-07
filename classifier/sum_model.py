77aaf20b7a49dde213190431ec61c780807cf116
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-07 10:48:08 +0100

    New model where word vectors are added.

---
+++ classifier/sum_model.py
@@ -0,0 +1,25 @@
+""" Similar to the convolutional model, but the convolutions are replaced
+with vector addition. """
+
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class Classifier(nn.Module):
+  def __init__(self, n_labels, n_dims, hidden_layer=200):
+    """ Initializes the model.
+    n_labels (int): no. of subjects in the classification problem.
+    n_dims (int): no. of dimensions of each input word.
+    """
+    super(Classifier, self).__init__()
+    self.fc1 = nn.Linear(n_dims, 200)
+    self.fc2 = nn.Linear(200, n_labels)
+
+  def forward(self, x):
+    """ Computes the forward pass.
+    x (tensor of size (batch_size x n_words x n_dims)): input.
+    The word vectors are summed and then fed to the feed-forward NN. """
+    x = F.relu(self.fc1(x.sum(1)))
+    return torch.sigmoid(self.fc2(x))
