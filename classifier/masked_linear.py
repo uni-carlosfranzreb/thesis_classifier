d2dcc5ec1360ff5b7cfcb74e9457acb3b75540e5
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-08 10:04:41 +0100

    Register mask as buffer
    It wll not be updated in the backward pass, but will passed to devices.

--- classifier/masked_linear.py
+++ classifier/masked_linear.py
@@ -1,5 +1,6 @@
 """ A masked version of the fully connected linear layer. The mask is a binary
-matrix, with ones for the connections that should be kept and else zeros. """
+matrix, with ones for the connections that should be kept and else zeros.
+From https://github.com/karpathy/pytorch-made/blob/master/made.py """
 
 
 from torch.nn import Linear
@@ -10,7 +11,7 @@ class MaskedLinear(Linear):
   """ same as Linear except has a configurable mask on the weights """
   def __init__(self, mask, in_features, out_features, bias=True):
     super().__init__(in_features, out_features, bias)        
-    self.mask = mask.T
+    self.register_buffer('mask', mask.T)
 
   def forward(self, input):
     return F.linear(input, self.mask * self.weight, self.bias)
