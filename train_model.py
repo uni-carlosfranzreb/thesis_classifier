fa51434f9ada361bd2cb4d7da051029f1a418f58
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-10 18:22:50 +0100

    Train model with even more dropout

--- train_model.py
+++ train_model.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     "subjects_file": 'data/openalex/subjects.json',
     "n_words": 250,
     "n_dims": 300,
-    "dropout": .4,
+    "dropout": .7,
     "loss": BCELoss(),
     "batch_size": 10,
     "n_epochs": 30,
