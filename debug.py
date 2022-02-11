8052f1e7865ad7c16570367857f8980b36935067
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-11 14:02:33 +0100

    Investigate an error on the server files

---
+++ debug.py
@@ -0,0 +1,21 @@
+""" Run this script in the server to investigate an error. """
+
+from os import listdir
+import json
+
+import torch
+
+folder = 'data/openalex/filtered_docs'
+for file in listdir(folder):
+  if 'test' in file:
+    continue
+  docs = json.load(open(f'{folder}/{file}', encoding='utf-8'))
+  for doc in docs:
+    if doc['data'] == []:  # doc has no words
+      continue
+    all_data = torch.tensor(doc['data'])
+    if all_data.shape[0] >= 250:
+      data = all_data[:250]
+    else:
+      data = torch.zeros(250, 300)
+      data[:all_data.shape[0]] = all_data
