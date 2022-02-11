c3f440734068d032164a154bd6ad8ada7c657625
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-11 14:04:41 +0100

    Print variables

--- debug.py
+++ debug.py
@@ -10,7 +10,9 @@ for file in listdir(folder):
   if 'test' in file:
     continue
   docs = json.load(open(f'{folder}/{file}', encoding='utf-8'))
+  print(docs.keys())
   for doc in docs:
+    print(doc)
     if doc['data'] == []:  # doc has no words
       continue
     all_data = torch.tensor(doc['data'])
