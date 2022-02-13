9fdd0fd9a4ba407907be3b005d9b970c6214a2e9
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-13 17:50:07 +0100

    Change epoch

--- get_assignments.py
+++ get_assignments.py
@@ -47,7 +47,7 @@ def prepare_data(data, n_words, n_dims):
   
 
 if __name__ == '__main__':
-  runs = [(1644599055, 19), (1644599121, 19)]
+  runs = [(1644599055, 20), (1644599121, 20)]
   for run_id, epoch in runs:
     model_file = f'data/classifiers/{run_id}/epoch_{epoch}.pt'
     dump_file = f'data/classifiers/{run_id}/probabilities.json'
