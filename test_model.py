c998159b8fbdb72b24fa05e96f7f8cb4d98349ab
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-19 12:24:49 +0100

    Test ASL

--- test_model.py
+++ test_model.py
@@ -3,18 +3,30 @@
 
 import torch
 
-from cnn.model import Classifier
+from cnn.convolutional_model import Classifier
+from cnn.asymmetric_loss import ASL
 
 
-def test_forward(n_subjects, batch_size, n_words, n_dims):
+def test_forward():
+  n_subjects = 1200
+  batch_size=10
+  n_words = 400
+  n_dims = 300
   x = torch.rand((batch_size, n_words, n_dims))
   model = Classifier(n_subjects, n_dims)
   model(x)
 
 
+def test_asl():
+  x = torch.tensor([.2, .4, .8])
+  y = torch.tensor([0, 1, 0])
+  bce = ASL(0, 0)
+  focal_loss = ASL(1, 1)
+  asl = ASL(2, 1)
+  print(bce(x, y))  # should be 0.9
+  print(focal_loss(x, y))  # should be 0.62
+  print(asl(x, y))  # should be 0.52
+  
+
 if __name__ == '__main__':
-  n_subjects = 1200
-  batch_size=10
-  n_words = 400
-  n_dims = 300
-  test_forward(n_subjects, batch_size, n_words, n_dims)
+  test_asl()
