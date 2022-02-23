db6a2cd9b307cea8a6f4c04174dfacdc5bd08800
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-02-23 18:02:26 +0100

    Train coherent model

--- train_model.py
+++ train_model.py
@@ -7,28 +7,33 @@ import logging
 from torch.nn import BCELoss
 from torch.optim import lr_scheduler
 
+from descendant_mask import create_mask 
+
 from cnn.init_training import init
 from cnn.asymmetric_loss import ASL
+from cnn.maxconstraint_loss import MCL
 from cnn.convolutional_model import Classifier as ConvClassifier
 from cnn.sum_model import Classifier as SumClassifier
+from cnn.coherent_model import Classifier as CoherentClassifier
 
 
 if __name__ == '__main__':
   """ Set the parameters for the training run.
-  - Models: ConvClassifier, SumClassifier.
-  - Losses: BCELoss, AsymmetricLossOptimized.
+  - Models: ConvClassifier, SumClassifier, CoherentClassifier.
+  - Losses: BCELoss, ASL or MCL.
   - Scheduler can also be None.
   - Optimizer can be 'SGD' or 'Adam'. """
+  subjects_file = 'data/openalex/subjects.json'
+  mask = create_mask(subjects_file)
   params = {
     "run_id": int(time()),
-    "model": ConvClassifier,
-    "subjects_file": 'data/openalex/subjects.json',
-    "docs_folder": 'data/openalex/split_docs',
-    "subjects_file": 'data/openalex/subjects.json',
+    "model": CoherentClassifier,
+    "subjects_file": subjects_file,
+    "docs_folder": 'data/openalex/debug_docs',
     "n_words": 250,
     "n_dims": 300,
     "dropout": .7,
-    "loss": BCELoss(),
+    "loss": MCL(mask),
     "batch_size": 10,
     "n_epochs": 20,
     "lr": .4,
@@ -40,7 +45,8 @@ if __name__ == '__main__':
     "shuffle": True,
     "hidden_layer": 100,
     "input_linear": 6200,
-    "model_state": 'data/classifiers/1645527301/epoch_20.pt'
+    "model_state": None,
+    "mask": mask
   }
   logging.basicConfig(
     level=logging.INFO,
